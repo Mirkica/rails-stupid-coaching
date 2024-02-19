87d14bf856ad617dd970504c0d20e20f02e427db
Mirela Mustic <mirela.mustic00@gmail.com>
2024-02-19 13:47:56 +0000

    added the answer view and implemented the answer on the answer page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,12 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    if params[:question] == 'I am going to work'
+      @response = 'Great!'
+    elsif params[:question].ends_with?('?')
+      @response = 'Silly question, get dressed and go to work!'
+    else
+      @response = "I don't care, get dressed and go to work!"
+    end
   end
 end
